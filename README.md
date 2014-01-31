b8dab11e44d70b3dbad636107dc4efa12b84d837
Ryan Czaja <ryanczaja@mac.com>
2014-01-31 18:24:55 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blocipedia
+
+A Wiki-style application 
+
+Made with my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = 'cb6623dbc022621692b0cd2de2344011c10233a2e86552dda05af023c567171d8d4a9062c8c5a7352a98348825274e15caf5e80816a9344f4af900cf2c7ff969'
